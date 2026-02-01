778fa9f74e7a579f16b91e4f5233b7b134bb1828
DeZhaysun <shuaihangtang@gmail.com>
2026-02-01 02:52:12 -0500

    updated descriptions

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -8,13 +8,12 @@ function MainSection() {
     <section className="main-section">
       <div className="intro">
         <img className="my-img" src={imgsrc} alt="ME" />
-        <h1 className="typing-text">Hello, I'm Jason!</h1>
-        <p className="small-text">I am a results-driven software developer experienced in full-stack app development, algorithms, and database management.</p>
-        <p className="small-text">Current Computer Science student at the University of Waterloo. Interested in Algorithms and Machine Learning.</p>
-        <p className="small-text"><b>Experience:</b> Software Developer at Trend Micro, Wind River, CoreAVI and QA at Peekapak</p>
-        <p className="small-text"><b>Languages:</b> Python, C++, JavaScript, C, Typescript, Java, SQL, HTML, CSS, Bash, Assembly, Groovy</p>
-        <p className="small-text"><b>Technologies:</b> FastAPI, MongoDB, AWS, Docker, Jenkins, PostgreSQL, React.js, React Native, Node.js, Express.js, Flask,
-WebSocket, Cypress, Linux, Git, NumpPy, Matplotlib, Jupyter Notebook, PyTorch, TensorFlow, Pandas, OpenAI API</p>
+        <h1 className="typing-text">Hello, I'm Jason Tang!</h1>
+        <p className="small-text">Computer Science student at the University of Waterloo (3.9 GPA), graduating 2026. Interested in algorithms and machine learning.</p>
+        <p className="small-text">Software engineer with full-stack, data, and infrastructure experience across Android, web, and cloud platforms.</p>
+        <p className="small-text"><b>Experience:</b> Meta, Loblaw Digital, Lynx Software Technologies, Wind River, Trend Micro</p>
+        <p className="small-text"><b>Languages:</b> Python, C++, TypeScript, JavaScript, C, Kotlin, Java, SQL, HTML, CSS, Bash, Assembly, Groovy</p>
+        <p className="small-text"><b>Technologies:</b> Git, FastAPI, MongoDB, AWS, GCP, Jenkins, PostgreSQL, React.js, React Native, Next.js, Node.js, Express.js, Flask, Linux, PyTorch, TensorFlow</p>
       </div>
     </section>
   );
